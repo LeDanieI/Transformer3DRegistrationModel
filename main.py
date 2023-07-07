52b1b9f40a053cc57d2992b72ecc71416fbd720d
Daniel Le <dle@live.nl>
2023-07-07 17:51:40 +0200

    Updated main.py

--- main.py
+++ main.py
@@ -2,6 +2,21 @@
 from datasets import OASISDataset
 from plotimg import plotimg
 
-test = OASISDataset()
-plotimg(test, 0)
+import torch
+import torch.utils.data as data
+
+
 #This is a test
+
+# torch.cuda.empty_cache()
+# torch.backends.cudnn.benchmark = True     
+learning_rate = 1e-9 # Tune this hyperparameter
+batch_size = 1
+epochs = 2
+device = 'cpu'
+
+
+
+dataset = OASISDataset()
+
+train_set, val_set, test_set = data.random_split(dataset,[0.7,0.1,0.2], generator=torch.Generator().manual_seed(42))
