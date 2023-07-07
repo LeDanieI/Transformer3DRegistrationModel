a657bb9dc42857d97d4b87dbaa84ab06e2069773
Daniel Le <dle@live.nl>
2023-07-07 17:59:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 ## Main
 from datasets import OASISDataset
 from plotimg import plotimg
-
-<<<<<<< HEAD
+from model.configurations import get_VitBase_config
 import torch
 import torch.utils.data as data
 
@@ -21,3 +20,10 @@ device = 'cpu'
 dataset = OASISDataset()
 
 train_set, val_set, test_set = data.random_split(dataset,[0.7,0.1,0.2], generator=torch.Generator().manual_seed(42))
+config = get_VitBase_config(img_size=tuple(dataset.inshape))
+model = RegTransformer(config)
+model = model.to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=True,num_workers=0,pin_memory=True)
+val_loader = torch.utils.data.DataLoader(val_set, batch_size=batch_size, shuffle=True,num_workers=0,pin_memory=True)
+test_loader = torch.utils.data.DataLoader(test_set, batch_size=batch
