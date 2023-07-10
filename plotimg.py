480a740f34f1391aaf7f742a55dccd578ec53612
Daniel Le <dle@live.nl>
2023-07-10 21:17:07 +0200

    Plotimg Updated

--- plotimg.py
+++ plotimg.py
@@ -3,10 +3,8 @@ import matplotlib.pyplot as plt
 
 def plotimg(dataset, index):
     fig, ax = plt.subplots()
-
-# figure(figsize=(4, 3), dpi=300)
-# plt.rcParams["axes.edgecolor"] = "black"
-# plt.rcParams["axes.linewidth"] = 2.50
-# p1,=plt.plot(mseu9, color='black')
-# p2,=plt.plot(mseu8, color='blue')
-# #plt.plot(mseu7, color='red')
+    figure(figsize=(4, 3), dpi=300)
+    plt.rcParams["axes.edgecolor"] = "black"
+    plt.rcParams["axes.linewidth"] = 2.50
+    p1,=plt.plot(mseu9, color='black')
+    p2,=plt.plot(mseu8, color='blue')
